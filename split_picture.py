de418a60bd4c729a5affb6c79168980acd6517ee
1 <https://github.com/zyd2000/Pix2Pix>
2022-12-03 23:35:03 +0800

    1

--- split_picture.py
+++ split_picture.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 
 
-path = "D:\\桌面\\实验报告\\深度学习\\picture\\拼接"  # 最后要加双斜杠，不然会报错
+path = "D:\\桌面\\实验报告\\深度学习\\picture\\new"  # 最后要加双斜杠，不然会报错
 filelist = os.listdir(path)
 
 
@@ -11,8 +11,9 @@ def main():
         whole_path = os.path.join(path, file)
         img = Image.open(whole_path)  # 打开图片img = Image.open(dir)#打开图片
         img = img.convert("RGB")  # 将一个4通道转化为rgb三通道
-        save_path = 'D:\\桌面\\实验报告\\深度学习\\picture\\new\\'
-        img.save(save_path + file)
+        crop = img.crop((0, 0, 256, 256))
+        save_path = 'D:\\桌面\\实验报告\\深度学习\\picture\\nn\\'
+        crop.save(save_path + file)
 
 
 if __name__ == "__main__":
